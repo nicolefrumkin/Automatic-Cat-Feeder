cdf38e7f0ba95d45839ab01e0aaf85153214f04c
yaronimon <yarons1@mail.tau.ac.il>
2025-06-29 16:35:20 +0300

    mqtt help

--- src/general.cpp
+++ src/general.cpp
@@ -68,7 +68,7 @@ void handleSerialCommands()
       Serial.println("   Add the Mosquitto installation folder (e.g.):");
       Serial.println("   C:\\Program Files\\mosquitto");
       Serial.println();
-      Serial.println("3. Open a new terminal window and run:");
+      Serial.println("3. Open a new terminal window, cd to project dirrctory and run:");
       Serial.println("   mosquitto_sub -h broker.hivemq.com -t \"catfeeder/#\" -v");
       Serial.println();
       Serial.println("You will now see live feeding and alert messages from your feeder.");
